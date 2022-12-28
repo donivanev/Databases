1ea5b84cb209f4ce9fbdb13472837f7058ba56c7
Doni <dok246@gmail.com>
2022-12-28 18:50:18 +0200

    Update PC11.sql

--- PC11.sql
+++ PC11.sql
@@ -4,24 +4,14 @@ SELECT * FROM laptop;
 SELECT * FROM pc;
 SELECT * FROM printer;
 
-/*
-1. —ъздайте изглед, който показва кодовете, моделите и цените на всички лаптопи, PC-та и принтери. 
-   Ќе премахвайте повторени€та.
-*/
-
 GO
-CREATE VIEW code_model_price AS SELECT code, model, price FROM pc UNION ALL
-SELECT code, model, price FROM laptop UNION ALL SELECT code, model, price FROM printer
+CREATE VIEW code_model_price AS SELECT code, model, price FROM pc UNION ALL SELECT code, model, price FROM laptop UNION ALL SELECT code, model, price FROM printer
 GO
 
 SELECT * FROM code_model_price;
 
--- 2. ѕроменете изгледа, като добавите и колона type (PC, Laptop, Printer)
--- 3. ѕроменете изгледа, като добавите и колона speed, ко€то е NULL за принтерите
-
 GO
-ALTER VIEW code_model_price AS SELECT code, model, price, speed, 'PC' AS type FROM pc UNION ALL 
-SELECT code, model, price, speed, 'Laptop' AS type FROM laptop UNION ALL 
+ALTER VIEW code_model_price AS SELECT code, model, price, speed, 'PC' AS type FROM pc UNION ALL SELECT code, model, price, speed, 'Laptop' AS type FROM laptop UNION ALL 
 SELECT code, model, price, NULL, 'Printer' AS type FROM printer;
 GO
 
