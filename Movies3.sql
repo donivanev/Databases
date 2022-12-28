9fd4396ad26c898ced4fa33dc15ed7ccdde1fd64
Doni <dok246@gmail.com>
2022-12-28 18:45:49 +0200

    Update Movies3.sql

--- Movies3.sql
+++ Movies3.sql
@@ -4,13 +4,6 @@ SELECT * FROM moviestar;
 SELECT * FROM movieexec;
 SELECT * FROM studio;
 
-/*
-3.1 Напишете заявка, която извежда имената на актрисите, които са също и продуценти с нетна стойност, 
-	по-голяма от 10 милиона.
-*/
-SELECT name FROM moviestar WHERE gender = 'F' AND name IN 
-(SELECT name FROM movieexec WHERE networth > 10000000);
-
---3.2 Напишете заявка, която извежда имената на тези филмови звезди, които не са продуценти.
+SELECT name FROM moviestar WHERE gender = 'F' AND name IN (SELECT name FROM movieexec WHERE networth > 10000000);
 
 SELECT name FROM moviestar WHERE name NOT IN (SELECT name FROM movieexec);
