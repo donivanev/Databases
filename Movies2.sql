a1d3fe0e891d44926ed3f535e3103366c7d5b4f4
Doni <dok246@gmail.com>
2022-12-28 18:45:26 +0200

    Update Movies2.sql

--- Movies2.sql
+++ Movies2.sql
@@ -2,7 +2,6 @@ SELECT * FROM movie;
 SELECT * FROM starsin;
 SELECT * FROM moviestar;
 
-SELECT name FROM moviestar JOIN starsin ON starname = name WHERE gender = 'F' AND 
-movietitle = 'Terms of Endearment';
+SELECT name FROM moviestar JOIN starsin ON starname = name WHERE gender = 'F' AND movietitle = 'Terms of Endearment';
 
 SELECT starname FROM starsin JOIN movie ON movietitle = title WHERE studioname = 'MGM' AND year = 1995;
