3706add699a74519cfc56cd5d2eae916267fdfcb
Doni <dok246@gmail.com>
2022-12-28 19:02:52 +0200

    Update Ships12.sql

--- Ships12.sql
+++ Ships12.sql
@@ -6,14 +6,8 @@ SELECT * FROM battles;
 
 BEGIN TRANSACTION;
 
-/*
-1. Ако бъде добавен нов клас с водоизместимост по-голяма от 35000, класът да бъде добавен в 
-   таблицата, но да му се зададе водоизместимост 35000.
-*/
-
 GO
-CREATE TRIGGER displacement35000 ON classes INSTEAD OF INSERT AS
-INSERT INTO classes SELECT class, type, country, numguns, bore, 35000 FROM inserted 
+CREATE TRIGGER displacement35000 ON classes INSTEAD OF INSERT AS INSERT INTO classes SELECT class, type, country, numguns, bore, 35000 FROM inserted 
 WHERE displacement > 35000
 GO
 
@@ -21,14 +15,8 @@ INSERT INTO classes VALUES ('asd', 'bb', 'Amerika', 7, 15, 50000)
 
 DROP TRIGGER displacement35000
 
-/*
-2. Създайте изглед, който показва за всеки клас името му и броя кораби (евентуално 0). 
-   Направете така, че при изтриване на ред да се изтрие класът и всички негови кораби.
-*/
-
 GO
-CREATE VIEW classShips AS SELECT classes.class, COUNT(name) as Count FROM classes JOIN ships 
-ON classes.class = ships.class GROUP BY classes.class;
+CREATE VIEW classShips AS SELECT classes.class, COUNT(name) as Count FROM classes JOIN ships ON classes.class = ships.class GROUP BY classes.class;
 GO
 
 SELECT * FROM classShips;
@@ -44,8 +32,6 @@ GO
 DELETE FROM classShips WHERE class LIKE 'Iowa';
 DROP TRIGGER deleteShipAndClass;
 
--- 3. Никой клас не може да има повече от два кораба.
-
 GO
 CREATE TRIGGER noClassWithMoreThan2Ships ON classes AFTER INSERT AS
 IF EXISTS (SELECT class FROM ships GROUP BY class HAVING COUNT(*) > 2)
@@ -56,11 +42,6 @@ END
 GO
 
 DROP TRIGGER noClassWithMoreThan2Ships;
-
-/*
-4. Кораб с повече от 9 оръдия не може да участва в битка с кораб, който е с по-малко от 9 оръдия. 
-   Напишете тригер за Outcomes.
-*/
 
 GO
 CREATE TRIGGER noMoreThan9Guns ON outcomes AFTER INSERT, UPDATE AS 
@@ -73,16 +54,10 @@ GO
 
 DROP TRIGGER noMoreThan9Guns;
 
-/*
-5. Кораб, който вече е потънал, не може да участва в битка, чиято дата е след датата на потъването му.
-   Напишете тригери за всички таблици, за които е необходимо.
-*/
-
 GO
 CREATE TRIGGER cantBattleAfterSunk ON outcomes AFTER INSERT, UPDATE AS
-IF EXISTS (SELECT * FROM outcomes JOIN battles b1 ON outcomes.battle = b1.name JOIN outcomes o2
-		   ON outcomes.ship = o2.ship JOIN battles b2 ON o2.battle = b2.name 
-		   WHERE outcomes.result = 'sunk' AND b1.date < b2.date)
+IF EXISTS (SELECT * FROM outcomes JOIN battles b1 ON outcomes.battle = b1.name JOIN outcomes o2 ON outcomes.ship = o2.ship JOIN battles b2 ON o2.battle = b2.name 
+   WHERE outcomes.result = 'sunk' AND b1.date < b2.date)
 BEGIN
 	RAISERROR('...', 16, 10);
 	ROLLBACK;
