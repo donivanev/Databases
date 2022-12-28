c404ddc85a3349dbede44019a555b1f5dd25ec52
Doni <dok246@gmail.com>
2022-12-28 18:34:27 +0200

    Update Movies11.sql

--- Movies11.sql
+++ Movies11.sql
@@ -5,18 +5,11 @@ SELECT * FROM moviestar;
 SELECT * FROM starsin;
 SELECT * FROM studio;
 
--- 1.Създайте изглед, който извежда имената и рождените дати на всички актриси.
-
 GO
 CREATE VIEW names_and_birthdates AS SELECT name, birthdate FROM moviestar WHERE gender = 'F';
 GO
 
 SELECT * FROM names_and_birthdates;
-
-/*
-2. Създайте изглед, който за всяка филмова звезда извежда броя на филмите, в които се е снимала. 
-   Ако за дадена звезда не знаем какви филми има, за нея да се изведе 0.
-*/
 
 GO
 CREATE VIEW stars_and_movies AS SELECT starname, COUNT(title) AS MoviesCount FROM starsin JOIN movie ON 
@@ -24,5 +17,3 @@ movietitle = title AND movieyear = year GROUP BY starname;
 GO
 
 SELECT * FROM stars_and_movies;
-
--- 3. Въпрос: горните изгледи позволяват ли модификации? - 
